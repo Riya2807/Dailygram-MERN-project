e442a86cd6bbe83e466e43d08a3a7a48fe62c930
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2022-08-05 01:43:44 +0530

    Update Profile.js

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,7 +1,21 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { UserContext } from '../../App'
 
 
 const Profile = ()=>{
+    const [mypics,setPics] = useState([])
+    const { state, dispatch } = useContext(UserContext)
+    useEffect(()=>{
+        fetch('/mypost',{
+            headers:{
+                'Authorization':"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            setPics(result.mypost)
+        })
+    },[])
+
     return(
         <div style={{maxWidth:'550px',margin:'0px auto'}}>
             <div style={{
@@ -16,18 +30,23 @@ const Profile = ()=>{
                     />
                 </div>
                 <div>
-                    <h4>Riya Godiyal</h4>
+                    
+                    <h4>{state?state.name:"loading"}</h4>
                     <div style={{display:'flex',justifyContent:'space-between', width:'108%'}}>
-                        <h6>358 posts</h6>
-                        <h6>271 followers</h6>
-                        <h6>300 following</h6>
+                        <h6>{mypics.length} posts</h6>
+                        <h6>{state?state.followers.length:"loading"} followers</h6>
+                        <h6>{state?state.following.length:"loading"} following</h6>
                     </div>
                 </div>
             </div>
             <div className='gallery'>
-                <img className='item' src='https://images.unsplash.com/photo-1642534270237-ae57b321c5bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8R0VoMVluRTdNLW98fGVufDB8fHx8&dpr=2&auto=format&fit=crop&w=294&q=60'/>
-                <img className='item' src='https://images.unsplash.com/photo-1588774587756-2d190b67c56c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60'/>
-                <img className='item' src='https://images.unsplash.com/photo-1612454376902-577cd469d008?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnxHRWgxWW5FN00tb3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60'/>
+            {
+                   mypics.map(item=>{
+                       return(
+                        <img key={item._id} className="item" src={item.photo} alt={item.title}/>  
+                       )
+                   })
+               }
 
             </div>
         </div>
