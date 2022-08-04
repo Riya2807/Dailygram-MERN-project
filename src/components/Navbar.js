6e4f6fde4f2ad6d5112806b5886d06d56460b064
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2022-08-05 01:42:05 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,39 @@
-import React from 'react'
-//import {Link} from 'react-router-dom'
+import React  from 'react'
 
 const NavBar = ()=>{
+    const renderList = ()=>{
+        if(localStorage.getItem("jwt")){
+            return [
+                <li><a href="/profile">Profile</a></li>,
+                <li><a href="/create">Create Post</a></li>,
+                <li>
+                    <button className="btn waves-effect waves-light #ea80fc purple darken-1"
+                onClick={()=>{
+                    localStorage.clear()
+                    window.location.href="http://localhost:3000/signin"
+                }}
+                >
+                    Logout   
+                </button>
+                </li>
+            ]
+        }
+        else{
+            return [
+                <li><a href="/signin">Signin</a></li>,
+                <li><a href="/signup">Signup</a></li>
+            ]
+        }
+    }
     return(
         <nav>
         <div className="nav-wrapper white">
             <a href="/" className="brand-logo left">Dailygram</a>
             <ul id="nav-mobile" className="right">
-                <li><a href="/signin">Signin</a></li>
-                <li><a href="/signup">Signup</a></li>
-                <li><a href="/profile">Profile</a></li>
-                <li><a href="/create">Create Post</a></li>
+                {renderList()}
             </ul>
         </div>
         </nav>
     )
 }
-
 export default NavBar
