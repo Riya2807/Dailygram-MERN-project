fb27f06d3c8a4b08a081941e501a693c4e350349
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2022-08-05 01:43:07 +0530

    Update Home.js

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,10 +1,10 @@
-//import { STATES } from 'mongoose'
-import { STATES } from 'mongoose'
-import React,{useState,useEffect} from 'react'
-//import {useContext} from '../../App'
+import React,{useState,useEffect, useContext} from 'react'
+import {UserContext} from '../../App'
+import {Link} from 'react-router-dom'
 
 const Home = ()=>{
     const [data,setData] = useState([])
+    const {state,dispatch} = useContext(UserContext)
     useEffect(()=>{
         fetch('/allpost',{
             headers:{
@@ -120,29 +120,42 @@ const Home = ()=>{
                 data.map(item=>{
                     return(
                         <div className='card home-card'>
-                            <h5>{item.postedBy.name}<i className='material-icons' style={{
+                            <h5><Link to={item.postedBy._id !== state._id?'/profile/'+item.postedBy._id:"/profile" }>{item.postedBy.name}</Link> {item.postedBy._id == state._id
+                            && <i className='material-icons' style={{
                                 float:"right"
                             }}
                             onClick={()=>deletePost(item._id)}
-                            >delete</i></h5>
+                            >delete</i>
+                            
+                            }</h5>
                             <div className='card-image'>
-                                <img src={item.photo} />
+                                <img src={item.photo} alt='Loading'/>
                             </div>
                             <div className='card-content'>
-                                
-                                <i className='material-icons'
-                                onClick={()=>{likePost(item._id)}}
-                                >thumb_up</i>
+                            
+                            {item.likes.includes(state._id)
+                            ? 
                                 <i className='material-icons'
                                 onClick={()=>{unlikePost(item._id)}}
                                 >thumb_down</i>
+                            :
+                                <i className='material-icons'
+                                onClick={()=>{likePost(item._id)}}
+                                >thumb_up</i>
+                            }
+
+
+                            
+                                
+                                
+                            
                                 <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 {
                                     item.comments.map(record=>{
                                         return(
-                                            <h6><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+                                            <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
                                         )
                                     })
                                 }
